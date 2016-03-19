f716541651956d80153d7372545ef448896ae272
Tim Reid <treid@spotxchange.com>
2016-03-19 13:37:33 +0000

    added a little config, http

--- src/test_timings/core.clj
+++ src/test_timings/core.clj
@@ -1,7 +1,24 @@
 (ns test-timings.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as http])
+  (:require [clojure.edn :as edn]))
+
+(def config
+  (edn/read-string (slurp "resources/config.edn")))
+
+
+(def default-headers
+  {:headers {:authentication (str "Bearer " (:access_token config))}})
+
+(def channel-url
+  (str (:base_url config) "/Channel"))
+
+(defn call-channel
+  ([] (http/get channel-url default-headers)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (:body response)))
+
+
--- test/test_timings/core_test.clj
+++ test/test_timings/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [test-timings.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-config-url
+  (testing "Reading config."
+    (is (= "http://local.api.spotxchange.com/1.0/Publisher(68800)" (:base_url config)))))
+
+
+(deftest test-config-token
+  (testing "Reading token from config."
+    (is (= "71ee02dfca317e558d9b52772d3fdd82842392f2" (:access_token config)))))
