65fb5a9912b6b01ae51f0d1c1bd175c29db9f9d6
Tim Reid <treid@spotxchange.com>
2016-03-24 11:27:33 +0000

    Added json; call sample service and extract timing info

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.0.0"]]
+                 [clj-http "2.0.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot test-timings.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/test_timings/core.clj
+++ src/test_timings/core.clj
@@ -1,24 +1,44 @@
 (ns test-timings.core
   (:gen-class)
   (:require [clj-http.client :as http])
+  (:require [clojure.data.json :as json])
   (:require [clojure.edn :as edn]))
 
 (def config
   (edn/read-string (slurp "resources/config.edn")))
 
+(defn build-url
+  [service]
+  (str (:base_url config) "/" service))
 
-(def default-headers
-  {:headers {:authentication (str "Bearer " (:access_token config))}})
+(defn channel-url [] (build-url "Channel"))
+(defn campaign-url [] (build-url "Campaign"))
 
-(def channel-url
-  (str (:base_url config) "/Channel"))
+(defn call-service
+  [url]
+  (http/get url
+            {:headers {:authorization (str "Bearer " (:access_token config))}}))
 
 (defn call-channel
-  ([] (http/get channel-url default-headers)))
+  ([] (call-service channel-url))
+  ([channel-id] (call-service (str channel-url "(" channel-id ")"))))
+
+(defn parse-json
+  [response key]
+  (get (json/read-str response) key))
+
+(defn is_enabled [channel_id] (parse-json (:body (call-channel channel_id)) "enabled"))
+
+(defn get-timing-map []
+  (let [resp (call-service (campaign-url))]
+    (let [headers (:headers resp)]
+      (filter (fn [x]
+                (.startsWith (key x) "x_timing")) headers))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (:body response)))
+  (println
+    (parse-json (:body (call-channel 75779)) "name")))
 
 
